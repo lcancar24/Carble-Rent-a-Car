245aa4843bae747d15b573d9bb5e658e105bea76
lcancar24 <lcancar24@foi.hr>
2026-05-22 12:28:14 +0200

    Novi probni commit sa dodanim modelima i repositorijima

---
+++ Carble Rent-a-Car/Models/Voditelj.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager.Models
+{
+    public abstract class Voditelj
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
