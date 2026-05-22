989fc03f5121f8f530920e80095a2b66e4d40b40
TECHNIQUESSS <92258780+TECHNIQUESSS@users.noreply.github.com>
2026-05-22 11:44:28 +0200

    Add project files.

---
+++ Carble Rent-a-Car/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Carble_Rent_a_Car
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
